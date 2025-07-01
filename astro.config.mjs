3c8ded0751d4240f9a2c1816b3210f59aede6646
Masputrawae <masputrawae.official@gmail.com>
2025-07-01 10:10:48 +0700

    Changed On 📅 : 2025-07-01 10:10:48

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 import icon from 'astro-icon'
 
+import remarkCallout from '@r4ai/remark-callout'
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://masputrawae.github.io',
@@ -15,5 +17,12 @@ export default defineConfig({
 		plugins: [tailwindcss()]
 	},
 
-	integrations: [icon()]
+	integrations: [icon()],
+
+	markdown: {
+		remarkPlugins: [remarkCallout],
+		shikiConfig: {
+			theme: 'gruvbox-dark-hard'
+		}
+	}
 })
