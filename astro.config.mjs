a7e604f5b86134971e548086ef0ed4667b9ca928
Masputrawae <masputrawae.official@gmail.com>
2025-07-09 14:07:30 +0700

    Changed On 📅 : 2025-07-09 14:07:30

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,15 +6,17 @@ import icon from 'astro-icon'
 import remarkCallout from '@r4ai/remark-callout'
 import pagefind from 'astro-pagefind'
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://masputrawae.github.io',
-  base: '/GASPOLL/',
-	vite: {
-		plugins: [tailwindcss()]
-	},
+  base: '/komit-atau-tidur/',
+    vite: {
+        plugins: [tailwindcss()]
+    },
 
-	integrations: [icon({iconDir: 'src/assets/icons'}, pagefind())],
+    integrations: [icon({iconDir: 'src/assets/icons'}), pagefind(), sitemap()],
 
   markdown: {
     remarkPlugins: [
